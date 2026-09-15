7cecceb89b1d0434d4620093b7d2b66b30841553
Nestor Raul Marchizone Barroso <nestormarchizone@gmail.com>
2026-09-15 19:07:12 -0300

    Class Alumno
    se crea la clase alumno

---
+++ src/Alumno.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nestor Marchizone
+ */
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    private HashSet<Materia> materias;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.materias = new HashSet<>();
+    }
+
+    public int getLegajo() { return legajo; }
+    public void setLegajo(int legajo) { this.legajo = legajo; }
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public void agregarMateria(Materia m) {
+        materias.add(m);
+    }
+
+    public int cantidadMaterias() {
+        return materias.size();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(legajo);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        return this.legajo == other.legajo;
+    }
+
+    @Override
+    public String toString() {
+        return apellido + " " + nombre + " (Legajo: " + legajo + ")";
+    }
+    
+}
